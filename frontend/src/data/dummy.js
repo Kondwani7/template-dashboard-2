9800d74ba7aba01fc8574325a99dbad2bdf61feb
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-03 22:33:32 +0200

    cart

--- frontend/src/data/dummy.js
+++ frontend/src/data/dummy.js
@@ -563,22 +563,19 @@ export const links = [
 
 export const cartData = [
   {
-    image:
-      product5,
+    image:product5,
     name: 'butterscotch ice-cream',
     category: 'Milk product',
     price: '$250',
   },
   {
-    image:
-      product6,
+    image:product6,
     name: 'Supreme fresh tomato',
     category: 'Vegetable Item',
     price: '$450',
   },
   {
-    image:
-      product7,
+    image:product7,
     name: 'Red color candy',
     category: 'Food Item',
     price: '$190',
